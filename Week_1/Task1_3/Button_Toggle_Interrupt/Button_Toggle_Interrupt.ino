743965330ca2ced6dcc61ff7693403de3682db24
Lachlan McInnes <lrmcinne@deakin.edu.au>
2019-03-14 20:14:30 +1000

    NEW: Task for 1_3

---
+++ Week_1/Task1_3/Button_Toggle_Interrupt/Button_Toggle_Interrupt.ino
@@ -0,0 +1,36 @@
+const int button1Pin = 2;
+const int button2Pin = 3;
+const int led1Pin =  13;
+const int led2Pin = 12;
+ 
+// variables will change:  
+int led1State = LOW;
+int led2State = LOW;
+ 
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(led1Pin, OUTPUT);
+  pinMode(led2Pin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(button1Pin, INPUT);
+  pinMode(button2Pin, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(button1Pin), toggleled1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(button2Pin), toggleled2, FALLING);
+}
+ 
+void loop() {
+
+  delay(500);
+}
+
+void toggleled1() {
+  led1State = !led1State;
+  digitalWrite(led1Pin, led1State);
+}
+
+void toggleled2() {
+  led2State = !led2State;
+  digitalWrite(led2Pin, led2State);
+}
+
