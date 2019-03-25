b9da8090c5299585c6123aa013960336ed353a2a
Lachlan McInnes <lrmcinne@deakin.edu.au>
2019-03-25 20:32:21 +1000

    NEW: Task 2_1

---
+++ Week_2/Task2_1/matrix.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+int main() {
+
+    cout << "Hello World!" << "\n";
+
+    int N = 200;
+
+    int a[N][N];
+    int b[N][N];
+
+    int i,j;
+
+    srand(time(NULL));
+
+    for (i = 0; i<N; i++ ){
+        for (j=0;j<N;j++){
+            a[i][j] = rand()%10+1;
+            b[i][j] = rand()%10+1;
+        }
+    }
+
+    int a_rows = sizeof(a)/sizeof(a[0]);
+    int a_cols = sizeof(a[0])/sizeof(a[0][0]);
+
+    int b_rows = sizeof(b)/sizeof(b[0]);
+    int b_cols = sizeof(b[0])/sizeof(b[0][0]);
+
+    if(a_cols == b_rows){
+        cout << "Multiplication is possible" << "\n";
+
+        int c[a_rows][b_cols];
+
+        int x,y,z;
+
+        auto start = high_resolution_clock::now();
+
+        for (x=0; x<a_rows; x++) {
+            for (y=0; y<b_cols; y++) {
+                c[x][y] = 0;
+                for (z=0; z<b_rows; z++) {
+                    c[x][y] += a[x][z]*b[z][y];
+                }
+            }
+        }
+
+        auto stop = high_resolution_clock::now();
+
+        auto duration = duration_cast<microseconds>(stop - start); 
+
+        /*
+        for(x=0;x<a_rows;x++)
+        {
+            for(y=0;y<b_cols;y++)
+            {
+                cout<<"\t"<<c[x][y];
+            }
+            cout<<"\n";
+        }
+        */
+
+        cout << "Execution time = " << duration.count() << " microseconds\n";
+
+    }
+    else{
+        cout << "Multiplication not possible" << "\n";
+    }
+
+    return 0;
+}
---
+++ Week_2/Task2_1/pthread_mat.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <pthread.h>
+
+using namespace std;
+using namespace std::chrono;
+
+#define N 200
+#define MAX_THREAD 2
+
+int a[N][N];
+int b[N][N];
+int c[N][N];
+int step_i = 0;
+
+void* multi(void *arg){
+    int core = step_i++;
+
+    for (int i = core*N/2; i<(core + 1)*N/2;i++){
+        for (int j = 0; j< N; j++){
+            for (int k = 0;k<N;k++){
+                c[i][j] += a[i][k]*b[k][j];
+            }
+        }
+    }
+
+    return 0;
+}
+
+int main() {
+
+    cout << "Hello World!" << "\n";
+
+    int i,j;
+
+    srand(time(NULL));
+
+    cout << "Creating matrix A:" << endl;
+    for (i = 0; i<N; i++ ){
+        
+        for (j=0;j<N;j++){
+            a[i][j] = rand()%10+1;
+            //cout << a[i][j] << "\t";
+        }
+        //cout << endl;
+    }
+
+    cout << "Creating matrix B:" << endl;
+    for (i = 0; i<N; i++ ){
+        
+        for (j=0;j<N;j++){
+            b[i][j] = rand()%10+1;
+            //cout << b[i][j] << "\t";
+        }
+        //cout << endl;
+    }
+
+    pthread_t threads[MAX_THREAD];
+
+    auto start = high_resolution_clock::now();
+
+    for (i=0;i<MAX_THREAD;i++){
+        int* p;
+        pthread_create(&threads[i],NULL,multi,(void*)(p));
+    }
+
+    for(i=0;i<MAX_THREAD;i++){
+        pthread_join(threads[i],NULL);
+    }
+
+    auto stop = high_resolution_clock::now();
+
+    /*
+    cout << endl << "Multiplication of the 2 random matrix:" << endl;
+    for (i=0;i<N;i++){
+        for (j=0;j<N;j++){
+            cout << c[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    */
+
+    auto duration = duration_cast<microseconds>(stop - start); 
+
+    cout << "\nDuration is :" << duration.count() << "microseconds\n";
+
+    return 0;
+
+}
